48d73ef6e9811abd9bc420ed0b42f7fb794c505d
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-04 15:45:00 +0200

    add: preparation odd or even

--- pariedispari/main.js
+++ pariedispari/main.js
@@ -1,1 +1,12 @@
-console.log("Funzioni?");
+//console.log("Funzioni?");
+
+/* L’utente sceglie pari o dispari e inserisce un numero da 1 a 5. Generiamo un numero random (sempre da 1 a 5) per il computer (usando una funzione). Sommiamo i due numeri Stabiliamo se la somma dei due numeri è pari o dispari (usando una funzione) Dichiariamo chi ha vinto. */
+
+//Preparazione
+/* - Prompt per far scegliere all'utente pari o dispari;
+   - Prompt per far scegliere all'utente un numero da 1 a 5;
+   - Funzione per generare numero random del pc;
+   - Sommare i due numeri;
+   - Definire somma pari o dispari;
+   - Dichiarare vincitore. 
+ */
