dfb0e118120ec0205991de1e2bad26e3e033b9bf
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-04 15:37:56 +0200

    add: function and if block

--- palindroma/main.js
+++ palindroma/main.js
@@ -9,6 +9,25 @@ Creare una funzione per capire se la parola inserita è palindroma */
    - Funzione per stabilire i parametri del dato inserito;
    - if per dare output diversi a seconda del dato (se parola inserita letta da sx a dx = se letta da dx a sx, allora è palindroma);
  */
+  /*  let word = prompt("Inserisci una parola");
+   let backwardWord = word.split("").reverse("").join(""); */
 
+
+function isTheWordAPalindrome(){
+    let word = prompt("Inserisci una parola:");
+    let backwardWord = word.split("").reverse("").join("");
+    let palindrom = word === backwardWord;
+
+    console.log(word);
+    console.log(backwardWord);
+
+    return palindrom;
+}
+
+if (isTheWordAPalindrome()){
+    console.log("La parola inserita è un palindromo!");
+} else {
+    console.log("La parola inserita non è un palindromo!");
+}
 
 
