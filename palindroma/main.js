eb52575ca7886eefffde1e3da3db019d0f5c95ad
EmilioDeChiara3388 <emilio.dechiara3388@gmail.com>
2024-10-04 14:55:41 +0200

    add: preparation

--- palindroma/main.js
+++ palindroma/main.js
@@ -1,1 +1,14 @@
-console.log("Funzioni?");
+//console.log("Funzioni?");
+
+
+/* Chiedere all’utente di inserire una parola 
+Creare una funzione per capire se la parola inserita è palindroma */
+
+//Preparazione:
+/* - Prompt per inserire la parola;
+   - Funzione per stabilire i parametri del dato inserito;
+   - if per dare output diversi a seconda del dato (se parola inserita letta da sx a dx = se letta da dx a sx, allora è palindroma);
+ */
+
+
+
